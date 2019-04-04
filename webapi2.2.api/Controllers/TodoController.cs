cc2ac6780c62f4ad996d35537d73d56bda7c9efc
vincentmelia <vincent.melia@gmail.com>
2019-04-04 13:14:05 -0400

    .

---
+++ webapi2.2.api/Controllers/TodoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi22.example.data_access.in_memory;
+using static webapi22.example.data_access.in_memory.DAL;
+using static webapi22.example.data_access.in_memory.MockDB;
+using webapi22.example.dtos.DtoClasses;
+using webapi22.example.dtos.DtoClasses.ToDoListWithTodosTypes;
+using webapi22.example.dtos.DtoClasses.UserTodoListsTypes;
+
+namespace webapi2._2.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        // GET: api/Todo
+        [HttpGet]
+        public UserTodoLists Get()
+        {
+           // return new string[] { "value1", "value2" };
+            return GetListsForUser(MockDB._userList[0].UserId);
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
