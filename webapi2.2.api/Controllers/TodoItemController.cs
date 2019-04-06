de8ccc002a6ed503c4cdc94e1536cb94ff76b91d
vincentmelia <vincent.melia@gmail.com>
2019-04-06 18:11:05 -0400

    mark complete route

--- webapi2.2.api/Controllers/TodoItemController.cs
+++ webapi2.2.api/Controllers/TodoItemController.cs
@@ -37,5 +37,23 @@ namespace webapi2._2.api.Controllers
             webapi22.example.data_access.DataAccess.AbstractDeleteSingleTodo(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId);
         }
 
+        //[HttpPut("{todoListId}/{todoListItemId}/MarkComplete")]
+        [Route("{todoListId}/{todoListItemId}/MarkComplete")]
+        public object /*ActionResult<TodoListItemDtoRow>**/ Put(Guid todoListId, Guid todoListItemId)
+        {
+            var todoToUpdate =
+                webapi22.example.data_access.DataAccess.AbstractGetSingleTodoItem(
+                    new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId);
+
+            todoToUpdate.TodoListItemIsComplete = true;
+
+            return webapi22.example.data_access.DataAccess.AbstractUpdateSingleTodoItem(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId, todoToUpdate);
+            //StatusCodes.Status406NotAcceptable
+            //var r = updatedTodoItem.ValidateTodoListItem();
+            //if (!r.Item1) return r.Item2.Select(i => i.ErrorMessage).ToList();
+
+            //return webapi22.example.data_access.DataAccess.AbstractUpdateSingleTodoItem(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId, updatedTodoItem);
+        }
+
     }
 }
