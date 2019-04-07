a492aa37813cb1fe85c2669d2a61e8edabed74b2
vincentmelia <vincent.melia@gmail.com>
2019-04-07 13:49:08 -0400

    .

--- webapi2.2.api/Controllers/TodoItemController.cs
+++ webapi2.2.api/Controllers/TodoItemController.cs
@@ -17,22 +17,26 @@ namespace webapi2._2.api.Controllers
         [HttpGet("{todoListId}/{todoListItemId}")]
         public ActionResult<Todo> Get(Guid todoListId, Guid todoListItemId)
         {
-            var r = MainValidator.Validate(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId);
+            var validationResults = MainValidator.Validate(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId);
 
-            if (r.Any(x => !x.Item1))
-                return BadRequest(r.Where(x => !x.Item1).ToList());
+            if (validationResults.Any(x => !x.Item1))
+                return BadRequest(validationResults.Where(x => !x.Item1).ToList());
 
             return Ok(webapi22.example.data_access.DataAccess.AbstractGetSingleTodoItem(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId));
         }
 
         [HttpPut("{todoListId}/{todoListItemId}")]
-        public object /*ActionResult<TodoListItemDtoRow>**/ Put(Guid todoListId, Guid todoListItemId, Todo updatedTodoItem)
+        public ActionResult<Todo> Put(Guid todoListId, Guid todoListItemId, Todo updatedTodoItem)
         {
             //StatusCodes.Status406NotAcceptable
-            var r = updatedTodoItem.ValidateTodoListItem();
-            if (!r.Item1) return r.Item2.Select(i => i.ErrorMessage).ToList();
+            var validationResults = MainValidator.Validate(updatedTodoItem,
+                new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId);
 
-            return webapi22.example.data_access.DataAccess.AbstractUpdateSingleTodoItem(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId, updatedTodoItem);
+            if (validationResults.Any(x => !x.Item1))
+                return BadRequest(validationResults.Where(x => !x.Item1).ToList());
+
+
+            return Ok(webapi22.example.data_access.DataAccess.AbstractUpdateSingleTodoItem(new Guid(HttpContext.Session.GetString("UserId")), todoListId, todoListItemId, updatedTodoItem));
         }
 
         [HttpDelete("{todoListId}/{todoListItemId}")]
