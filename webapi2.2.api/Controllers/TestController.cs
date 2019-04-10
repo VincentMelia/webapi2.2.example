c8a5b93d6dec769979e737140ca5165406ff443d
vincentmelia <vincent.melia@gmail.com>
2019-04-10 17:10:51 -0400

    .

---
+++ webapi2.2.api/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi2._2.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("throwerror")]
+        public object Get()
+        {
+            throw new Exception("Test global error handler");
+        }
+
+    }
+}
